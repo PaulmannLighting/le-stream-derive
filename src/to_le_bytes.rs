b3f1b472b1dd25cedf2b22d3bd318740863982f9
Richard Neumann <neumann@paulmann.de>
2023-11-17 08:21:08 +0100

    Remove confusing template comments

--- src/to_le_bytes.rs
+++ src/to_le_bytes.rs
@@ -4,21 +4,12 @@ use quote::quote;
 use syn::{parse_macro_input, parse_quote, Data, DeriveInput, Fields};
 
 pub fn to_le_bytes(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    // Parse the input tokens into a syntax tree.
     let input = parse_macro_input!(input as DeriveInput);
-
-    // Used in the quasi-quotation below as `#name`.
     let name = input.ident;
-
-    // Add a bound `T: ToLeBytes` to every type parameter T.
     let generics = add_trait_bounds(input.generics, &parse_quote!(le_stream::ToLeBytes));
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
-
-    // Generate an expression to sum up the heap size of each field.
     let (iterator_statement, iterator_type) = impl_body(&input.data);
-
     let expanded = quote! {
-        // The generated impl.
         impl #impl_generics le_stream::ToLeBytes for #name #ty_generics #where_clause {
             type Iter = #iterator_type;
 
@@ -28,7 +19,6 @@ pub fn to_le_bytes(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
     };
 
-    // Hand the output tokens back to the compiler.
     proc_macro::TokenStream::from(expanded)
 }
 
